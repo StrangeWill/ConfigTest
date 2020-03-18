79934bf5d9ed910ee1f49ededdcc8894e1282bab
William Roush <william.roush@roushtech.net>
2020-03-18 13:23:19 -0400

    Stuff

---
+++ AppSettings.cs
@@ -0,0 +1,9 @@
+namespace ConfigTest
+{
+    public class AppSettings
+    {
+        public string Test { get; set; }
+
+        public int Test2 { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace configtest
+{
+    using System;
+    using System.IO;
+    using ConfigTest;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Options;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            var serviceProvider = new ServiceCollection()
+                .Configure<AppSettings>(o => configuration.Bind(o))
+                .BuildServiceProvider();
+
+            var appSettings = serviceProvider.GetService<IOptions<AppSettings>>().Value;
+
+
+            Console.WriteLine($"{appSettings.Test} {appSettings.Test2}");
+
+        }
+    }
+}
